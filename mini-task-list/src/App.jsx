4f6ab64fbc72bfe51e6bf857807869687766e05a
faraon2000 <ramzurval@gmail.com>
2025-07-24 01:45:32 +0200

    m-t-l-updated

--- mini-task-list/src/App.jsx
+++ mini-task-list/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
-import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 
 const App = () => {
-	const image = '../src/images/free-code-camp.jpg';
 	return (
 		<div className='app-task'>
 			<div className='freecodecamp-logo-box'>
-				<img src={image} className='freecodecamp-logo' />
+				<h1 className='title'>Mini Tasks List</h1>
 			</div>
 			<div className='main-task-list'>
 				<h1>Tasks</h1>
